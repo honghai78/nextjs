47d23816e578d87586ca58fff6d4df752e924743
Hai Tran <honghaitran84@gmail.com>
2018-11-08 11:42:49 +0700

    no message

--- bin/travis_deploy.sh
+++ bin/travis_deploy.sh
@@ -1,8 +1,10 @@
 #!/bin/bash -l
 
 if [ "$TRAVIS_BRANCH" == "staging-firebase" ]; then
+  yarn
   travis_wait 20 yarn deploy-staging
 elif [ "$TRAVIS_BRANCH" == "production-firebase" ]; then
+  yarn
   travis_wait 20 yarn deploy-production
 else
   echo "Branch is $TRAVIS_BRANCH. Not deploying..."
